0978673a897b4798dcf520a30e74706bae2e1873
Archie Wheeler <archwheeler@gmail.com>
2019-11-24 15:15:06 +0000

    Create Logico class
    The basic logico has a strength value and an amount of wealth.

---
+++ src/Logico.java
@@ -0,0 +1,17 @@
+public class Logico {
+  private int strength;
+  private int wealth;
+
+  public Logico(int strength) {
+    wealth = 0;
+    this.strength = strength;
+  }
+
+  public int getStrength() {
+    return strength;
+  }
+
+  public int getWealth() {
+    return wealth;
+  }
+}
